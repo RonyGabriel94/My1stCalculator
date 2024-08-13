c2bb1de301c4fd91098821cf2b515ba99fe91dc1
RonyGabriel94 <rgcb.1994@outlook.com>
2024-08-13 22:36:35 +0100

    Add files via upload

---
+++ My1stCalculator/My1stCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please add a whole number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please add another whole number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int addTotal = num1 + num2;
+int subtractTotal = num1 - num2;
+int multipliedTotal = num1 * num2;
+int divideTotal = num1 / num2;
+
+Console.WriteLine(addTotal);
+Console.WriteLine(subtractTotal);
+Console.WriteLine(multipliedTotal);
+Console.WriteLine(divideTotal);
